ecd5b628f78633d7bde14cdccddbaf3f38bd5564
Rajaul <uddin.rajaul1@gmail.com>
2024-09-05 15:52:21 +0545

    Comments

--- main.py
+++ main.py
@@ -9,11 +9,17 @@ api = FastAPI()
 
 @api.post('/remove-background')
 async def remove_background(file: UploadFile=File(...)):
+    # read uploaded image file
     input_image = Image.open(BytesIO(await file.read()))
 
-    output_image = remove(input_image)
+    #remove bg using rembg
+    output_image = remove(input_image, alpha_matting=True, alpha_matting_foreground_threshold=270,alpha_matting_background_threshold=20, alpha_matting_erode_size=11)
 
+    # save file by giving temp name
     output_filename = f"{uuid.uuid4()}.png"
+
+    # save file
     output_image.save(output_filename,format="png")
 
+    # return file response
     return FileResponse(output_filename, media_type='image/png', filename=output_filename)
