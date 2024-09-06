b4a7cea1d56f761bca5db50dd2f44c2fc9fc823b
Rajaul <uddin.rajaul1@gmail.com>
2024-09-06 10:51:22 +0545

    Accept multiple files

--- main.py
+++ main.py
@@ -1,25 +1,47 @@
 from fastapi import FastAPI, File, UploadFile
-from rembg import remove
-from fastapi.responses import FileResponse
+from rembg import remove, new_session
+from fastapi.responses import FileResponse, JSONResponse
 from io import BytesIO
 from PIL import Image
 import uuid
+import os
 
 api = FastAPI()
 
+
+model_name = "u2net"
+rembg_session = new_session(model_name)
+
+# Get the absolute path for the output directory
+output_dir = os.path.join(os.getcwd(), "images")
+os.makedirs(output_dir, exist_ok=True)
+
 @api.post('/remove-background')
-async def remove_background(file: UploadFile=File(...)):
-    # read uploaded image file
-    input_image = Image.open(BytesIO(await file.read()))
+async def remove_background(files: list[UploadFile]=File(...)):
 
-    #remove bg using rembg
-    output_image = remove(input_image, alpha_matting=True, alpha_matting_foreground_threshold=270,alpha_matting_background_threshold=20, alpha_matting_erode_size=11)
+    output_files = []
 
-    # save file by giving temp name
-    output_filename = f"{uuid.uuid4()}.png"
+    for file in files:
+        # save file by giving temp name
+        output_filename = os.path.join(output_dir, f"{uuid.uuid4()}.png")
 
-    # save file
-    output_image.save(output_filename,format="png")
+        # read uploaded image file
+        input_image = await file.read()
 
-    # return file response
-    return FileResponse(output_filename, media_type='image/png', filename=output_filename)
+        # Remove background using rembg with the session
+        output_image = remove(input_image, session=rembg_session,
+                              alpha_matting=True,
+                              alpha_matting_foreground_threshold=270,
+                              alpha_matting_background_threshold=20,
+                              alpha_matting_erode_size=11)
+        with open(output_filename, 'wb') as o:
+            o.write(output_image)
+
+        output_files.append(output_filename)
+
+    if len(output_files) == 1:
+        # return file response
+        return FileResponse(output_files[0], media_type='image/png', filename=output_files[0])
+
+
+    return JSONResponse({"processed_files":  output_files})
